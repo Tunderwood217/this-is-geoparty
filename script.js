a0ded0ca7730438926ad75bc22d0592f4bb933c2
Tiffani Underwood <tdunderwood217@gmail.com>
2023-10-14 14:46:16 -0400

    added js to hide questions and show results

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var beginButton = document.querySelector("#begin");
 var startScreen = document.querySelector("#welcome");
 var questionEl =document.querySelector("#question");
+var questionSubmit = document.querySelector("#submit");
+var quizEnd = document.querySelector("#result");
 
-beginButton.addEventListener("click", function(){
+beginButton.addEventListener("click",  onBegin);
+
+function onBegin() {
     startScreen.classList.add("hidden");
     questionEl.classList.remove("hidden");
-})
-    
+}
 
+questionSubmit.addEventListener("click", onSubmit);
+    
+function onSubmit() {
+    questionEl.classList.add("hidden");
+    quizEnd.classList.remove("hidden");
+}
