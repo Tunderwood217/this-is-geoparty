3167db1dd128c4b74b0854e0806c96dd532b185d
Tiffani Underwood <tdunderwood217@gmail.com>
2023-10-14 14:36:58 -0400

    used js to hide welcome screen and view q1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+var beginButton = document.querySelector("#begin");
+var startScreen = document.querySelector("#welcome");
+var questionEl =document.querySelector("#question");
+
+beginButton.addEventListener("click", function(){
+    startScreen.classList.add("hidden");
+    questionEl.classList.remove("hidden");
+})
+    
 
